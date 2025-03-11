952465618ead6d514a547d9a3c3f8883203a053d
n1cef <108896986+n1cef@users.noreply.github.com>
2025-03-11 15:51:10 +0100

    Update dependency.h
    headers

--- include/dependency.h
+++ include/dependency.h
@@ -1,6 +1,6 @@
 #ifndef DEPENDENCY_H
 #define DEPENDENCY_H
-#include "/usr/include/graph.h"
+#include "/usr/kraken/include/graph.h"
 
 void resolve_dep(Graph *graph);
 void install_pkg_dfs(Node *node);
